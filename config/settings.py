fe2a8529923abac1358daa93e9ae97287d9ebdab
Rami19994 <ramirkab223@gmail.com>
2026-04-30 00:04:31 +0300

    Fix: Handle Vercel read-only filesystem for SQLite

--- config/settings.py
+++ config/settings.py
@@ -63,6 +63,8 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 if os.environ.get("POSTGRES_DB"):
     DATABASES = {"default": {"ENGINE": "django.db.backends.postgresql", "NAME": os.environ.get("POSTGRES_DB"), "USER": os.environ.get("POSTGRES_USER"), "PASSWORD": os.environ.get("POSTGRES_PASSWORD"), "HOST": os.environ.get("POSTGRES_HOST", "localhost"), "PORT": os.environ.get("POSTGRES_PORT", "5432")}}
+elif os.environ.get("VERCEL"):
+    DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": os.path.join("/tmp", "db.sqlite3")}}
 else:
     DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}}
 
